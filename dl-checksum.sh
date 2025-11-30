19d0f6c3247275407ace7f54f6cdce944dc54688
Andrew Rothstein <andrew.rothstein@gmail.com>
2025-11-29 22:48:20 -0500

    opa:1.11.0

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -11,14 +11,9 @@ dl()
     local suffix=${4:-}
     local platform="${os}_${arch}"
     local file=opa_${os}_${arch}${suffix}
-    local lfile=$DIR/opa_${platform}_${ver}${suffix}
-    local url=$MIRROR/$ver/$file
-    if [ ! -e $lfile ];
-    then
-        curl -sSLf -o $lfile $url
-    fi
+    local url=$MIRROR/$ver/$file.sha256
     printf "    # %s\n" $url
-    printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
+    printf "    %s: sha256:%s\n" $platform $(curl -sSLf $url | awk '{print $1}')
 }
 
 dl_ver() {
@@ -32,4 +27,16 @@ dl_ver() {
     dl $ver windows amd64 .exe
 }
 
-dl_ver ${1:-v1.3.0}
+dl_ver v1.4.0
+dl_ver v1.4.1
+dl_ver v1.4.2
+dl_ver v1.5.0
+dl_ver v1.5.1
+dl_ver v1.6.0
+dl_ver v1.7.0
+dl_ver v1.7.1
+dl_ver v1.8.0
+dl_ver v1.9.0
+dl_ver v1.10.0
+dl_ver v1.10.1
+dl_ver ${1:-v1.11.0}
